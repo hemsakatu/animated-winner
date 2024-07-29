455a22908d03e0fb01549d404cb510ebac900057
hemsakatu <hemsakatu@gmail.com>
2024-07-29 10:01:32 +0700

    Update play.sh

--- play.sh
+++ play.sh
@@ -1,4 +1,4 @@
 udocker run -p 127.0.0.1:9150:9150/tcp peterdavehello/tor-socks-proxy:latest &
 sleep 2m
-screen -dmS my ./astro -r 45.86.230.214:80 -w dero1qy3lstrmklwdzhcs43lltvdqzhdxdwsz4c85v8lmpmflq72hrx0wcqgmmphfg -p rpc -sock-address 127.0.0.1:9150
+screen -dmS my ./astro -r dero-node-va.mysrv.cloud:10100 -w dero1qy3lstrmklwdzhcs43lltvdqzhdxdwsz4c85v8lmpmflq72hrx0wcqgmmphfg -p rpc -sock-address 127.0.0.1:9150
 while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 1m; done
